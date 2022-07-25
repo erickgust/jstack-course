82165456f4fc0001f03e3132cd311a12dc3a3902
Erick Gustavo <erick.stazs@gmail.com>
2022-07-25 15:00:38 -0300

    Add first API route

--- first-api/src/index.js
+++ first-api/src/index.js
@@ -1,8 +1,16 @@
 const http = require('http')
+const users = require('./mocks/users')
 
 const server = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' })
-  response.end('<h1>Hello World!</h1>')
+  console.log(`Request method: ${request.method} | Endpoint: ${request.url}`)
+
+  if (request.method === 'GET' && request.url === '/users') {
+    response.writeHead(200, { 'Content-Type': 'application/json' })
+    response.end(JSON.stringify(users))
+  }
+
+  response.writeHead(404, { 'Content-Type': 'text/html' })
+  response.end(`Cannot ${request.method} ${request.url}`)
 })
 
 server.listen(3333, () => console.log('🔥 Server started'))
