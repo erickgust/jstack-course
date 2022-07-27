aa0a378b5d688e2f2f7a3791732c52fb17f5e555
Erick Gustavo <erick.stazs@gmail.com>
2022-07-27 17:09:27 -0300

    Get parameters in the route

--- first-api/src/index.js
+++ first-api/src/index.js
@@ -5,15 +5,28 @@ const PORT = 3333
 
 const server = http.createServer((request, response) => {
   const parsedUrl = new URL(`http://localhost:${PORT}${request.url}`)
+  let { pathname } = parsedUrl
+  let id = null
 
   console.log(`Request method: ${request.method} | Endpoint: ${parsedUrl.pathname}`)
 
+  const splitEndpoint = pathname.split('/').filter(Boolean)
+
+  if (splitEndpoint.length > 1) {
+    pathname = `/${splitEndpoint[0]}/:id`
+    id = splitEndpoint[1]
+  }
+
+  console.log('splitEndpoint', splitEndpoint, 'pathname', pathname)
+
   const route = routes.find((routeObj) => (
-    routeObj.endpoint === parsedUrl.pathname && routeObj.method === request.method
+    routeObj.endpoint === pathname && routeObj.method === request.method
   ))
 
   if (route) {
     request.query = Object.fromEntries(parsedUrl.searchParams)
+    request.params = { id }
+
     route.handler(request, response)
   } else {
     response.writeHead(404, { 'Content-Type': 'text/html' })
