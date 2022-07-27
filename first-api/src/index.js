efc8c340875f57b167d154dd780ec08ba6991383
Erick Gustavo <erick.stazs@gmail.com>
2022-07-26 23:17:34 -0300

    Add query params example

--- first-api/src/index.js
+++ first-api/src/index.js
@@ -1,19 +1,24 @@
 const http = require('http')
 const routes = require('./routes')
+const { URL } = require('url')
+const PORT = 3333
 
 const server = http.createServer((request, response) => {
-  console.log(`Request method: ${request.method} | Endpoint: ${request.url}`)
+  const parsedUrl = new URL(`http://localhost:${PORT}${request.url}`)
+
+  console.log(`Request method: ${request.method} | Endpoint: ${parsedUrl.pathname}`)
 
   const route = routes.find((routeObj) => (
-    routeObj.endpoint === request.url && routeObj.method === request.method
+    routeObj.endpoint === parsedUrl.pathname && routeObj.method === request.method
   ))
 
   if (route) {
+    request.query = Object.fromEntries(parsedUrl.searchParams)
     route.handler(request, response)
   } else {
     response.writeHead(404, { 'Content-Type': 'text/html' })
-    response.end(`Cannot ${request.method} ${request.url}`)
+    response.end(`Cannot ${request.method} ${parsedUrl.pathname}`)
   }
 })
 
-server.listen(3333, () => console.log('🔥 Server started'))
+server.listen(PORT, () => console.log('🔥 Server started'))
