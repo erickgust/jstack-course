ff57f955a92eed065112cd388b517965c789cbec
Erick Gustavo <erick.stazs@gmail.com>
2022-07-31 18:57:42 -0300

    Add show and delete routes

--- my-contacts/src/app/controllers/ContactController.js
+++ my-contacts/src/app/controllers/ContactController.js
@@ -6,8 +6,16 @@ class ContactController {
     response.json(contacts);
   }
 
-  show() {
-    // Obter UM registro
+  async show(request, response) {
+    const { id } = request.params;
+
+    const contact = await ContactsRepository.findById(id);
+
+    if (!contact) {
+      return response.status(404).json({ error: 'User not found' });
+    }
+
+    response.json(contact);
   }
 
   store() {
@@ -18,8 +26,19 @@ class ContactController {
     // Editar um registro
   }
 
-  delete() {
-    // Deletar um registro
+  async delete(request, response) {
+    const { id } = request.params;
+
+    const contact = await ContactsRepository.findById(id);
+
+    if (!contact) {
+      return response.status(404).json({ error: 'User not found' });
+    }
+
+    await ContactsRepository.delete(id);
+
+    // 204: No Content
+    response.sendStatus(204);
   }
 }
 
--- my-contacts/src/app/repositories/ContactsRepository.js
+++ my-contacts/src/app/repositories/ContactsRepository.js
@@ -1,6 +1,6 @@
 const { v4 } = require('uuid');
 
-const contacts = [
+let contacts = [
   {
     id: v4(),
     name: 'Erick',
@@ -8,11 +8,31 @@ const contacts = [
     phone: '999999999',
     category: v4(),
   },
+  {
+    id: v4(),
+    name: 'Mateus',
+    email: 'mateus@mail.com',
+    phone: '888888888',
+    category: v4(),
+  },
 ];
 
 class ContactsRepository {
   findAll() {
-    return new Promise((resolve) => { resolve(contacts); });
+    return new Promise((resolve) => resolve(contacts));
+  }
+
+  findById(id) {
+    return new Promise((resolve) => resolve(
+      contacts.find((contact) => contact.id === id),
+    ));
+  }
+
+  delete(id) {
+    return new Promise((resolve) => {
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve();
+    });
   }
 }
 
