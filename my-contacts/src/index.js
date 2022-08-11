0b895788833bf7363acab4b68c04c621c03faca8
Erick Gustavo <erick.stazs@gmail.com>
2022-08-11 10:29:02 -0300

    Add error handler

--- my-contacts/src/index.js
+++ my-contacts/src/index.js
@@ -5,5 +5,10 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use((error, request, response, next) => {
+  console.log('=== Error Handler ===');
+  console.log(error);
+  response.sendStatus(500);
+});
 
 app.listen(3333, () => console.log('🔥 Server started at http://localhost:3333 🔥'));
