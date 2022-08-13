e83db8e90e882436e5f06788b1365e89113c71fd
Erick Gustavo <erick.stazs@gmail.com>
2022-08-13 11:31:04 -0300

    Add html-webpack-plugin

--- react-fundamentos/webpack.config.js
+++ react-fundamentos/webpack.config.js
@@ -1,9 +1,16 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: path.resolve(__dirname, 'transpiled', 'index.js'),
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle-[hash].js',
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+      minify: false
+    })
+  ]
 }
