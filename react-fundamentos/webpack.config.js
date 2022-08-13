c2f014468789e36f8ffaf53d9b855849aae7b7f5
Erick Gustavo <erick.stazs@gmail.com>
2022-08-13 14:14:43 -0300

    Add clean-webpack-plugin

--- react-fundamentos/webpack.config.js
+++ react-fundamentos/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
   entry: path.resolve(__dirname, 'transpiled', 'index.js'),
@@ -11,6 +12,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public', 'index.html'),
       minify: false
-    })
+    }),
+    new CleanWebpackPlugin(),
   ]
 }
