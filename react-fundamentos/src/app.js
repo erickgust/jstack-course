86ff4d9f59c994a4a43987364f4bfe1929d6688f
Erick Gustavo <erick.stazs@gmail.com>
2022-08-14 19:59:00 -0300

    Add props example

--- react-fundamentos/src/app.js
+++ react-fundamentos/src/app.js
@@ -1,12 +1,32 @@
 import React from 'react'
+import { Post } from './post'
 
-const category = 'Posts da semana'
+const posts = [
+  {
+    title: 'Título da notícia 1',
+    subtitle: 'Subtítulo da notícia 1',
+  },
+  {
+    title: 'Título da notícia 2',
+    subtitle: 'Subtítulo da notícia 2',
+  },
+  {
+    title: 'Título da notícia 3',
+    subtitle: 'Subtítulo da notícia 3',
+  },
+]
 
 export function App() {
   return (
     <>
       <h1>JStack's Blog</h1>
-      <h1>{category}</h1>
+      <h1>Posts da semana</h1>
+
+      <hr />
+
+      {posts.map((post, index) =>(
+        <Post {...post} key={index} />
+      ))}
     </>
   )
 }
---
+++ react-fundamentos/src/post.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export function Post(props) {
+  return (
+    <>
+      <article>
+        <strong>{props.title}</strong>
+        <br />
+        <small>{props.subtitle}</small>
+      </article>
+      <br />
+    </>
+  )
+}
