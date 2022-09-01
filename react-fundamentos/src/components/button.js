f14ab337cfee4e39b3aa293ac2e23b47237d4cc3
Erick Gustavo <erick.stazs@gmail.com>
2022-09-01 10:20:47 -0300

    Move button.js to components

--- react-fundamentos/src/components/button.js
+++ react-fundamentos/src/components/button.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import PropTypes from 'prop-types'
-import { ThemeContext } from './contexts/theme'
+import { ThemeContext } from '../contexts/theme'
 
 export function Button({ children, onClick }) {
   const { theme } = useContext(ThemeContext)
