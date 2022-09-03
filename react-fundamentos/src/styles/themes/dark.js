48deaa4d27a7157ceb587212649dda329caf49b1
Erick Gustavo <erick.stazs@gmail.com>
2022-09-03 10:31:15 -0300

    Add default theme colors

--- react-fundamentos/src/styles/themes/dark.js
+++ react-fundamentos/src/styles/themes/dark.js
@@ -3,5 +3,7 @@ import commonStyles from './common'
 export default {
   ...commonStyles,
   backgroundColor: '#222',
+  backgroundLayer1: '#111',
+  postBackgroundColor: '#333',
   textColor: '#fff'
 }
--- react-fundamentos/src/styles/themes/light.js
+++ react-fundamentos/src/styles/themes/light.js
@@ -3,5 +3,7 @@ import commonStyles from './common'
 export default {
   ...commonStyles,
   backgroundColor: '#fff',
+  backgroundLayer1: '#ccc',
+  postBackgroundColor: '#eee',
   textColor: '#222'
 }
