29f2a3af4a46bcd60d1f159a1114bc53d54110d8
Erick Gustavo <erick.stazs@gmail.com>
2022-09-04 16:27:57 -0300

    Persist selected theme on localStorage

--- react-fundamentos/src/contexts/theme.js
+++ react-fundamentos/src/contexts/theme.js
@@ -1,4 +1,4 @@
-import React, { createContext, useMemo, useState } from 'react'
+import React, { createContext, useEffect, useMemo, useState } from 'react'
 import { ThemeProvider } from 'styled-components'
 import PropTypes from 'prop-types'
 
@@ -7,10 +7,15 @@ import themes from '../styles/themes'
 export const ToggleThemeContext = createContext(null)
 
 export function ToggleThemeProvider ({ children }) {
-  const [theme, setTheme] = useState('dark')
+  const storedTheme = localStorage.getItem('theme')
+  const [theme, setTheme] = useState(storedTheme ?? 'dark')
 
   const currentTheme = useMemo(() => {
     return themes[theme] || themes.dark
+  }, [theme])
+
+  useEffect(() => {
+    localStorage.setItem('theme', theme)
   }, [theme])
 
   function handleToggleTheme () {
