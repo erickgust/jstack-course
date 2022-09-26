409e9d434a25d594bab0965369390489cbdaf002
Erick Gustavo <erick.stazs@gmail.com>
2022-09-25 21:43:09 -0300

    Add navigation bar with Link

--- react-router-v5/src/components/layout/layout.js
+++ react-router-v5/src/components/layout/layout.js
@@ -3,14 +3,20 @@ import React from 'react'
 import { Header } from '../header'
 import { Footer } from '../footer'
 import { Routes } from '../../routes'
+import { BrowserRouter, Link } from 'react-router-dom'
+import { Nav } from './layout-styles'
 
 function Layout() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
+      <Nav>
+        <Link to='/'>Home</Link>
+        <Link to='/posts'>Posts</Link>
+      </Nav>
       <Routes />
       <Footer />
-    </>
+    </BrowserRouter>
   )
 }
 
--- react-router-v5/src/routes.js
+++ react-router-v5/src/routes.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import { Home } from './pages/home'
 import { Posts } from './pages/posts/posts'
 
 function Routes() {
   return (
-    <BrowserRouter>
+    <>
       <Route path='/' component={Home} exact />
       <Route path='/posts' component={Posts} />
-    </BrowserRouter>
+    </>
 
   )
 }
