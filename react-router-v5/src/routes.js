33cb4d0e24f3112e0ada4bea129cceb90f2a090f
Erick Gustavo <erick.stazs@gmail.com>
2022-09-26 09:10:03 -0300

    Add NotFound route

--- react-router-v5/src/routes.js
+++ react-router-v5/src/routes.js
@@ -1,14 +1,16 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import { Home } from './pages/home'
 import { Posts } from './pages/posts/posts'
+import { NotFound } from './pages/not-found'
 
 function Routes() {
   return (
-    <>
+    <Switch>
       <Route path='/' component={Home} exact />
       <Route path='/posts' component={Posts} />
-    </>
+      <Route path='*' component={NotFound} />
+    </Switch>
 
   )
 }
