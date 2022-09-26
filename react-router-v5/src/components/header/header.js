852c7438786a75456237e2fa48fb9abf6efb1f7f
Erick Gustavo <erick.stazs@gmail.com>
2022-09-26 14:42:13 -0300

    Add useHistory example

--- react-router-v5/src/components/header/header.js
+++ react-router-v5/src/components/header/header.js
@@ -1,13 +1,27 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 import { useToggleTheme } from '../../hooks/useToggleTheme'
 import { Container } from './header-styles'
 
-function Header () {
+function Header() {
   const { theme, handleToggleTheme } = useToggleTheme()
+  const history = useHistory()
+
+  function handleNavigate() {
+    history.push('/posts')
+  }
 
   return (
     <Container>
       <h1>JStack's Blog</h1>
+
+      <button
+        onClick={handleNavigate}
+        style={{ color: '#FFF' }}
+      >
+        Ir para posts
+      </button>
+
       <button
         type="button"
         onClick={handleToggleTheme}
--- react-router-v5/src/pages/post/post.js
+++ react-router-v5/src/pages/post/post.js
@@ -6,8 +6,6 @@ function Post() {
   const { search } = useLocation()
   const query = useMemo(() => new URLSearchParams(search), [search])
 
-  console.log(query.get('query'))
-
   return (
     <h1>Post {id}</h1>
   )
